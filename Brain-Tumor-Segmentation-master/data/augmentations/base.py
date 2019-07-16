e8a3614f88cc958d84a892d6fb4fa5be66065da2
Leow Zheng Xu <>
2019-07-16 14:20:51 +0800

    kj

--- Brain-Tumor-Segmentation-master/data/augmentations/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AugmentorBase(ABC):
-
-    @abstractmethod
-    def co_transform(self, batch_data, batch_label, **kwargs):
-        pass
--- Brain-Tumor-Segmentation-master/models/batch_samplers/utils.py
+++
@@ -1,6 +0,0 @@
-from itertools import chain
-
-
-def flatten(listOfLists):
-    "Flatten one level of nesting"
-    return list(chain.from_iterable(listOfLists))
